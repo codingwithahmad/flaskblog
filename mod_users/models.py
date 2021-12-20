3faf5dec885d89cc4f15b3d6e30dcf7c13cd905a
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-12-20 21:52:30 +0330

    v15-m5:00

--- mod_users/models.py
+++ mod_users/models.py
@@ -2,8 +2,14 @@ from sqlalchemy import Column, Integer, String
 from app import db
 
 class User(db.Model):
+
+	__tablename__="users"
+
 	id = Column(Integer(), primary_key=True)
 	email = Column(String(128), nullable=False, unique=True)
 	password = Column(String(128), nullable=False)
 	role = Column(Integer(), nullable=False, default=0)
 	full_name = Column(String(128), nullable=True)
+
+	def set_password(self, password):
+		pass
