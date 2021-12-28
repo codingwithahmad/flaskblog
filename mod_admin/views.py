6c2e988af9d84c17f000a88f4460746d0894244d
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-12-28 22:03:54 +0330

    Login view is 40% completed and video 18 m20

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,5 +1,6 @@
-from flask import session, render_template, request
+from flask import session, render_template, request, abort
 from mod_users.forms import LoginForm
+from mod_users.models import User
 from . import admin
 
 @admin.route('/')
@@ -11,5 +12,9 @@ def index():
 def login():
 	form = LoginForm(request.form)
 	if request.method == 'POST':
-		pass
+		if not form.validate_on_submit():
+			abort(400)
+		user = User.query.filter(User.email.ilike(f'{form.email.data}')).first()
+		if not user:
+			return "Incorrect Credentials", 400			
 	return render_template("admin/login.html", form=form)
