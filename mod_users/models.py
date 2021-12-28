4d083a282b96c59f507bba15fddc0c6295d2ffc8
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-12-28 22:02:40 +0330

    add check_password def to User model

--- mod_users/models.py
+++ mod_users/models.py
@@ -14,3 +14,6 @@ class User(db.Model):
 
 	def set_password(self, password):
 		self.password = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password, password)
