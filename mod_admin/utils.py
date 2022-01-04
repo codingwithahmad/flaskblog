2fc291688fa538f4cf7a37c72bccf3dcf1551535
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-01-04 23:00:29 +0330

    change proccess_view decorateor name to only_admin and develop that

--- mod_admin/utils.py
+++ mod_admin/utils.py
@@ -1,6 +1,15 @@
 from flask import session, abort
 
-def protected_view(func):
+def admin_only(func):
 	def decorator(*args, **kwargs):
 		if session.get('user_id') is None:
 			abort(401)
+
+		if session.get('role') != 1:
+			abort(403)
+
+		return func(*args, **kwargs)
+
+
+
+	return decorator
