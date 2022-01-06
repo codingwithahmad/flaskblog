8936f5578a22585d06744f233da23c1a44e080ac
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-01-06 18:49:25 +0330

    create a wtf form for registr and named it RegisterForm

--- mod_users/forms.py
+++ mod_users/forms.py
@@ -1,7 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import EmailField, PasswordField
+from wtforms.fields import EmailField, PasswordField, StringField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
 	email = EmailField(validators=[DataRequired()])
 	password = PasswordField(validators=[DataRequired()])
+
+
+class RegisterForm(FlaskForm):
+
+	full_name = StringField()
+
+	email = EmailField(validators=[DataRequired()])
+	password = PasswordField(validators=[DataRequired()])
+	password_confirm = PasswordField(validators=[DataRequired()])
