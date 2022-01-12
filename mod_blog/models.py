f8500d25427c217c816a6cf57130292c60f6256d
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-01-12 21:42:13 +0330

    add Post model to models.py in mod_blog

--- mod_blog/models.py
+++ mod_blog/models.py
@@ -1,11 +1,20 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text
 
 from app import db
 
 
 class Category(db.Model):
-	id = Column(Integer, primary_key)  
+	__tablename__ = "categories"
+	id = Column(Integer, primary_key=True)  
 	name = Column(String(128), nullable=False, unique=True)
 	desc = Column(String(256), nullable=True, unique=False)
+	slug = Column(String(128), nullable=False, unique=True)
+
+class Post(db.Model):
+	__tablename__ = "posts"
+	id = Column(Integer, primary_key=True)  
+	title = Column(String(128), nullable=False, unique=True)
+	summery = Column(String(256), nullable=True, unique=False)
+	content = Column(Text, nullable=False, unique=False)
 	slug = Column(String(128), nullable=False, unique=True)
 		
