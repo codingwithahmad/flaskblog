15c9574fa84dc1641b9b8b0e2db4ce8eb9bae4ff
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-16 22:37:30 +0330

    add smtp config for sending email from gmail and create an object of Mail named mail

--- config.py
+++ config.py
@@ -2,14 +2,19 @@ import os
 
 
 class Config:
-	SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_PORT= os.getenv('MAIL_PORT')
+    MAIL_USE_TLS= os.getenv('MAIL_USE_TLS')
 
 
 class Development(Config):
-	DEBUG = True
+    DEBUG = True
 
 
 class Production(Config):
-	DEBUG = False
+    DEBUG = False
