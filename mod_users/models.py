536709a634fdd9c2526e5d648961f91197d969f3
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-19 22:55:51 +0330

    add active field to User model and then migrate and upgrade db

--- mod_users/models.py
+++ mod_users/models.py
@@ -1,23 +1,24 @@
 from werkzeug.security import check_password_hash, generate_password_hash
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from app import db
 
 class User(db.Model):
 
-	__tablename__="users"
+    __tablename__="users"
 
-	id = Column(Integer(), primary_key=True)
-	email = Column(String(128), nullable=False, unique=True)
-	password = Column(String(128), nullable=False)
-	role = Column(Integer(), nullable=False, default=0)
-	full_name = Column(String(128), nullable=True)
+    id = Column(Integer(), primary_key=True)
+    email = Column(String(128), nullable=False, unique=True)
+    password = Column(String(128), nullable=False)
+    role = Column(Integer(), nullable=False, default=0)
+    full_name = Column(String(128), nullable=True)
+    active = Column(Boolean, nullable=False, unique=False, default=False)
 
-	def set_password(self, password):
-		self.password = generate_password_hash(password)
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
-	def check_password(self, password):
-		return check_password_hash(self.password, password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
-	def is_admin(self):
-		return self.role == 1
+    def is_admin(self):
+        return self.role == 1
