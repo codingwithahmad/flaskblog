aff40ff2a517857711966732804e183299424955
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-21 22:40:03 +0330

    get token with add_to_redis function in register view after commit then send message with send_signup_message to new user email

--- mod_users/views.py
+++ mod_users/views.py
@@ -3,36 +3,39 @@ from sqlalchemy.exc import IntegrityError
 
 
 from . import users
+from .utils import add_to_redis, send_signup_message
 from .forms import RegisterForm
 from app import db
 from .models import User
 
 @users.route('/register/', methods=['GET', 'POST'])
 def register():
-	form = RegisterForm()
-	if request.method == 'POST':
-		if not form.validate_on_submit():
-			return render_template('users/register.html', form=form)
-		if not form.password.data == form.password_confirm.data:
-			error_message = "Password and Confirm Password dosn't match"
-			form.password.errors.append(error_message)
-			form.password_confirm.errors.append(error_message)
+    form = RegisterForm()
+    if request.method == 'POST':
+        if not form.validate_on_submit():
+            return render_template('users/register.html', form=form)
+        if not form.password.data == form.password_confirm.data:
+            error_message = "Password and Confirm Password dosn't match"
+            form.password.errors.append(error_message)
+            form.password_confirm.errors.append(error_message)
 
-			return render_template('users/register.html', form=form)
+            return render_template('users/register.html', form=form)
 
 
-		new_user = User()
-		new_user.full_name = form.full_name.data
-		new_user.email = form.email.data
-		new_user.set_password(form.password.data)
-		try:
-			db.session.add(new_user)
-			db.session.commit()
-			flash('You create your account successfully', 'success')
-		except IntegrityError:
-			db.session.rollback()
-			form.email.errors.append('This email is registered, please use another email')
-			return render_template('users/register.html', form=form)			
-	
+        new_user = User()
+        new_user.full_name = form.full_name.data
+        new_user.email = form.email.data
+        new_user.set_password(form.password.data)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            token = add_to_redis(new_user, 'register')
+            send_signup_message(new_user, token)
+            flash('You create your account successfully', 'success')
+        except IntegrityError:
+            db.session.rollback()
+            form.email.errors.append('This email is registered, please use another email')
+            return render_template('users/register.html', form=form)
 
-	return render_template('users/register.html', form=form)
+
+    return render_template('users/register.html', form=form)
